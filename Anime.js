369ebd763e63c05cbe201b0b1700ba6dc5157683
Dinakarr114 <buchinellydinakar07@gmail.com>
2025-06-26 19:54:04 +0530

    Initial commit

---
+++ Anime.js
@@ -0,0 +1,21 @@
+
+//   const images = [
+//     "../Anime/Photos/Luffy.jpeg", "../Anime/Photos/Luffy.jpeg", "../Anime/Photos/Luffy.jpeg", "../Anime/Photos/Luffy.jpeg", "../Anime/Photos/Luffy.jpeg"
+//   ];
+
+//   const container = document.getElementById("Scroll-Div");
+
+//   // Add original images
+//   images.forEach(src => {
+//     const img = document.createElement("img");
+//     img.src = src;
+//     container.appendChild(img);
+//   });
+
+//   // Add duplicate for seamless scroll
+//   images.forEach(src => {
+//     const img = document.createElement("img");
+//     img.src = src;
+//     container.appendChild(img);
+//   });
+
